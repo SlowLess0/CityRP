8521c9a0b4b58c1140cebd9ed9c46a8f1216407b
SlowLess0 <83511426+SlowLess0@users.noreply.github.com>
2021-05-01 21:45:27 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,60 @@
+const Discord = require('discord.js'),
+    client = new Discord.Client({
+        fetchAllMembers: true,
+        partials: ['MESSAGE', 'REACTION']
+    }),
+    config = require('./config.json'),
+    fs = require('fs')
+ 
+client.login(config.token)
+client.commands = new Discord.Collection()
+client.db = require('./db.json')
+ 
+fs.readdir('./commands', (err, files) => {
+    if (err) throw err
+    files.forEach(file => {
+        if (!file.endsWith('.js')) return
+        const command = require(`./commands/${file}`)
+        client.commands.set(command.name, command)
+    })
+})
+ 
+client.on('message', message => {
+    if (message.type !== 'DEFAULT' || message.author.bot) return
+
+    if(message.guild) {
+        if (!message.member.hasPermission('MANAGE_CHANNELS') && client.db.lockedChannels.includes(message.channel.id)) return message.delete()
+    }
+ 
+    const args = message.content.trim().split(/ +/g)
+    const commandName = args.shift().toLowerCase()
+    if (!commandName.startsWith(config.prefix)) return
+    const command = client.commands.get(commandName.slice(config.prefix.length))
+    if (!command) return
+    if (command.guildOnly && !message.guild) return message.channel.send('Cette commande ne peut être utilisée que dans un serveur.')
+    command.run(message, args, client)
+})
+ 
+client.on('guildMemberAdd', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(`${member}`, new Discord.MessageEmbed()
+        .setDescription(`${member} a rejoint le serveur. Nous sommes désormais ${member.guild.memberCount} ! 🎉`)
+        .setColor('#00ff00'))
+    member.roles.add(config.greeting.role)
+})
+ 
+client.on('guildMemberRemove', member => {
+    member.guild.channels.cache.get(config.greeting.channel).send(new Discord.MessageEmbed()
+        .setDescription(`${member.user.tag} a quitté le serveur... 😢`)
+        .setColor('#ff0000'))
+})
+
+client.on('channelCreate', channel => {
+    if (!channel.guild) return
+    const muteRole = channel.guild.roles.cache.find(role => role.name === 'Mutet')
+    if (!muteRole) return
+    channel.createOverwrite(muteRole, {
+        SEND_MESSAGES: false,
+        CONNECT: false,
+        ADD_REACTIONS: false
+    })
+})
